da8fad36133dabaa434b474b2cef49980719be5e
G <53386620+Gnola@users.noreply.github.com>
2020-03-21 16:08:43 -0700

    set up temporary landing page

--- client/src/components/TempLanding.js
+++ client/src/components/TempLanding.js
@@ -6,7 +6,7 @@ class TempLanding extends Component {
       <div className='landing'>
         <h1>Welcome to </h1>
           <h1 id='brand'>Phillips Construction</h1>
-        <h2>This site is currently under constrution but will be live shortly!</h2>
+        <h2>This site is currently under construction but will be live shortly!</h2>
         <h3>In the meantime, feel free to contact Jason at:</h3>
         <div className='landing-links'>
           <a href='#' target='_blank'>(707) 812-5516</a>
