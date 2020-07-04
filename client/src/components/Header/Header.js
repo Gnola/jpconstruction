5a2581a75068ad6e1af9c9a53a566f9106e8074c
G <53386620+Gnola@users.noreply.github.com>
2020-07-04 13:49:43 -0700

    styled header and nav for mobile

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,13 +1,14 @@
 import React from 'react';
 // import Logo1 from './headerimgs/Logo.png';
 // import Logo2 from './headerimgs/Logo2.png';
-import Logo3 from './headerimgs/Logo3.png';
+// import Logo3 from './headerimgs/Logo3.png';
+import Logo4 from './headerimgs/Logo4.png';
 
 // HEADER
 const Header = (props) => {
   return (
     <div id='Header' className='Header'>
-      <img id='Logo' alt='Logo' src={Logo3} onClick={()=>props.changePage('home')}/>
+      <img id='Logo' alt='Logo' src={Logo4} onClick={()=>props.changePage('home')}/>
     </div>
   )
 }
