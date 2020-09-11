3a3356f6aa56bfe9c32d4a056cc8e69720ed9cc7
G <53386620+Gnola@users.noreply.github.com>
2020-09-11 11:34:29 -0700

    added contact form no email error handling

--- client/src/components/Home/ContactLanding/Form.js
+++ client/src/components/Home/ContactLanding/Form.js
@@ -8,7 +8,8 @@ class Form extends Component {
     email:'',
     phone:'',
     subject:'',
-    inquiry:''
+    inquiry:'',
+    error: ''
   }
 
   handleChange = (e) => {
@@ -17,32 +18,36 @@ class Form extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state);
+    if (this.state.email === '') {
+      this.setState({error:'Please include a valid email address'})
+    } else {
+      const { name, email, phone, subject, inquiry} = this.state
+      console.log(name, email, phone, subject, inquiry);
 
-    const { name, email, phone, subject, inquiry} = this.state
+      let templateParams = {
+        name,
+        email,
+        phone,
+        subject,
+        inquiry
+       }
+       console.log(templateParams);
 
-    let templateParams = {
-      name,
-      email,
-      phone,
-      subject,
-      inquiry
-     }
-
-    emailjs.send('gmail', 'inquiry', templateParams, 'user_m4C35RBt84FZzFDpPhTSG')
-    .then((result) => {
-        console.log(result.text);
-    }, (error) => {
-        console.log(error.text);
-    });
-    this.props.sent()
-    this.setState({
-      name:'',
-      email:'',
-      phone:'',
-      subject:'',
-      inquiry:''
-    })
+      emailjs.send('gmail', 'inquiry', templateParams, 'user_m4C35RBt84FZzFDpPhTSG')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      this.props.sent()
+      this.setState({
+        name:'',
+        email:'',
+        phone:'',
+        subject:'',
+        inquiry:''
+      })
+    }
   }
 
   render() {
@@ -50,16 +55,17 @@ class Form extends Component {
       <form id='form' className='ContactForm' onSubmit={this.handleSubmit}>
         <div className='ContactFormInfo'>
           <span><label>Name:</label> <input type='text' id='name' value={this.state.name} onChange={this.handleChange} /></span>
-          <span><label>Email:</label> <input type='email' id='email' placeholder='example@example.com' value={this.state.email} onChange={this.handleChange} /></span>
+          <span><label>*Email:</label> <input type='email' id='email' placeholder='example@example.com' value={this.state.email} onChange={this.handleChange} /></span>
           <span><label>Phone:</label> <input type='phone' id='phone' placeholder='(555) 555-555' value={this.state.phone} onChange={this.handleChange} /></span>
         </div>
         <div className='ContactFormInquire'>
           <span><label>Subject:</label> <input type='text' id='subject' placeholder={this.props.subject} value={this.state.subject} onChange={this.handleChange} /></span>
           <label id='inquire-label'>Inquiry</label>
           <textarea type='text' id='inquiry' onChange={this.handleChange} value={this.state.inquiry} placeholder={`I would like to know more about your ${this.props.subject ? this.props.subject : 'services'}`}/>
+          {this.state.error === '' ? null : <p id='error-message'>{this.state.error}</p>}
         </div>
         <div className='ContactFormButton'>
-          <input type='submit' value='Send'></input>
+          <button className={this.state.email === '' ? 'disabled' : 'active'} >Send</button>
         </div>
       </form>
     );
