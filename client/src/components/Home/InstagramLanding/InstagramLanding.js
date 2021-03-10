d38b5545ef3351a7b832b86a8a7fd74db2ad930f
G <53386620+Gnola@users.noreply.github.com>
2021-03-10 12:48:02 -0800

    Updated Instagram feed to v2.0 because it was breaking the site

--- client/src/components/Home/InstagramLanding/InstagramLanding.js
+++ client/src/components/Home/InstagramLanding/InstagramLanding.js
@@ -6,7 +6,7 @@ const InstagramLanding = (props) => {
   return (
     <div id='InstagramLanding'>
       <h3>Check out our <a href='https://www.instagram.com/jphillipsconstruction/' target='_blank' rel="noopener noreferrer">Instagram</a> for some recent projects!</h3>
-      <Feed id='Feed' userName="jphillipsconstruction" className="Feed" classNameLoading="Loading" limit="8"/>
+      <Feed id='Feed' userName="jphillipsconstruction" className="Feed" classNameLoading="Loading" limit="9"/>
     </div>
   )
 }
